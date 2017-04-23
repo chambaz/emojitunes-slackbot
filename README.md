02b509f3cd1b5b3383c4a363c39fa2f101d6ae2d
Adam Chambers <adammchambers@gmail.com>
2017-04-23 10:58:15 -0400

    Initial project commit

---
+++ README.md
@@ -0,0 +1,9 @@
+😎 EmojiTunes Slackbot 🤖
+========================
+
+A Slack Bot to communicate with the EmojiTunes API
+
+**Installation**
+
+	$ npm install
+	$ node start
---
+++ index.js
@@ -0,0 +1,109 @@
+// require dependencies
+const request = require('superagent')
+const emoji = require('node-emoji')
+const msgs = require('./lib/msgs')
+const Botkit = require('botkit')
+const token = process.env.SLACK_TOKEN
+const controller = Botkit.slackbot({
+  retry: Infinity,
+  debug: true
+})
+
+// assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  controller.spawn({
+    token: token
+  }).startRTM(err => {
+    if (err) {
+      throw new Error(err)
+    }
+  })
+
+// otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  require('beepboop-botkit').start(controller, { debug: true })
+}
+
+// reply to a direct mention - @bot hello
+controller.on('mention', handleMessage)
+
+// reply to a direct mention - @bot hello
+controller.on('direct_mention', handleMessage)
+
+// reply to a direct message
+controller.on('direct_message', handleMessage)
+
+function handleMessage(bot, message) {
+  let foundEmoji = ''
+
+  // loop through each word searching for an emoji
+  message.text.split(' ').every(word => {
+    let w = word.trim()
+
+    // test if word starts and ends with :
+    // in which case convert to emoji first
+    if (w.startsWith(':') && w.endsWith(':')) {
+      w = emoji.get(w)
+    }
+
+    // test for emoji and break when one found
+    if (emoji.which(w)) {
+      foundEmoji = word
+      return false
+    }
+
+    return true
+  })
+
+  // no emoji found so let user know
+  if (!foundEmoji) {
+    console.log('No emoji found in message')
+    bot.reply(message, msgs.getNoEmojiMsg())
+
+  // emoji found so send recommendations back
+  } else {
+    console.log('Found emoji!', foundEmoji)
+    fetchRecommendationsForChannel(bot, message, foundEmoji)
+  }
+}
+
+// fetch recommendations from emojitunes API
+// pass data on and send recommendation to channel
+function fetchRecommendationsForChannel(bot, message, emoji) {
+  console.log('Making request...')
+
+  request
+      .get(`https://emojitunes.io/api/recommendations/${emoji}`)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) {
+          console.log('Error getting recommendations', err)
+          return
+        }
+
+        const json = JSON.parse(res.text)
+
+        // error from API so let user know
+        if (err || json.error) {
+          bot.reply(message, msgs.getNoResultsMsg())
+          return
+        }
+
+        sendRecommendationToChannel(bot, message, json.tracks[0].url, emoji)
+      })
+}
+
+// send a recommendation to a channel
+function sendRecommendationToChannel(bot, message, url, emoji) {
+	// we need the recommendation URL
+  if (!url) {
+    return false
+  }
+
+	// if no message specified then grab one from msgs module based on emoji
+  const msg = msgs.getRecommendationMsg(emoji)
+
+	// send message to channel followed by recommendation URL
+  bot.reply(message, msg)
+  setTimeout(() => bot.reply(message, url), 1000)
+}
